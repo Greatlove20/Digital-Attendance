0d1f41e07d71d742c4159207a6ff6c0615ee275f
Jeremiah Dornu <jeremiahdornu70@gmail.com>
2026-03-01 00:01:46 +0000

    Added Student class with basic attributes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <iostream>
+#include <string>
 using namespace std;
+
+class Student {
+public:
+    string indexNumber;
+    string name;
+    string program;
+
+    void display() {
+        cout << indexNumber << " " << name << " " << program << endl;
+    }
+};
 
 int main(){
     cout << "Digital Attendance System\n";
